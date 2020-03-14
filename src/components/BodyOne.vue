a14d23bcbb812b46bbb295b7856df64197eb1557
polindinh <polin_dinh88@web-posting.us-central1-a.c.sunlit-plasma-264816.internal>
2020-03-14 14:26:05 +0000

    remove hover effect and added click effect

--- src/components/BodyOne.vue
+++ src/components/BodyOne.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="body-one" v-on:mouseover="mouseover"
+    <div id="body-one" v-on:click="mouseclick"
     v-on:mouseleave="mouseleave">
         <!-- <p v-on:mouseover="mouseover"
     v-on:mouseleave="mouseleave">{{msg}}</p> -->
@@ -22,8 +22,8 @@ export default {
         msg:String
     },
     methods:{
-        mouseover: function(){
-            this.msg = "You hovered over me! Now move"
+        mouseclick: function(){
+            this.msg = "Who told you to click me! Now move out of the box so can can get back to what i was showing before"
         },
         mouseleave: function(){
             this.msg="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque totam, reiciendis praesentium et quod, temporibus, a ipsam expedita facilis nisi facere voluptatem. Nulla reiciendis quos fugit corrupti. Nostrum, reprehenderit eos."
